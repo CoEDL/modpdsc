962f90654137fad2ea3aafa2c3a8c54248528ccf
Marco La Rosa <m@lr.id.au>
2022-06-23 03:00:51 +1000

    revise

--- ui/src/main.js
+++ ui/src/main.js
@@ -8,7 +8,7 @@ config.autoReplaceSvg = "nest";
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./routes";
+import { router } from "./routes";
 import { store } from "./store";
 import ElementPlus from "element-plus";
 import log from "loglevel";
@@ -22,36 +22,24 @@ prefixer.apply(log);
 import HTTPService from "./http.service";
 
 (async () => {
-    // let response = await fetch("/api/configuration");
-    // if (response.status === 200) {
-    //     let configuration = await response.json();
-    //     store.commit("saveConfiguration", { ...configuration });
-    //     // Vue.config.productionTip = false;
-    //     const app = createApp(App);
-    //     app.use(store);
-    //     app.use(router({ configuration }));
-    //     app.use(ElementPlus);
-    //     app.config.globalProperties.$http = new HTTPService({ router });
-    //     app.provide("$http", app.config.globalProperties.$http);
-    //     app.config.globalProperties.$log = log;
-    //     app.provide("$log", app.config.globalProperties.$log);
-    //     // app.config.globalProperties.$socket = io();
-    //     // app.provide('$socket', app.config.globalProperties.$socket)
-    //     app.mount("#app");
-    // }
+    let response = await fetch("/api/configuration");
+    if (response.status === 200) {
+        let configuration = await response.json();
+        store.commit("saveConfiguration", { ...configuration });
+        //     // Vue.config.productionTip = false;
+        const app = createApp(App);
+        app.use(store);
+        app.use(router({ configuration }));
+        app.use(ElementPlus);
+        app.config.globalProperties.$http = new HTTPService({ router });
+        app.provide("$http", app.config.globalProperties.$http);
+        app.config.globalProperties.$log = log;
+        app.provide("$log", app.config.globalProperties.$log);
+        //     // app.config.globalProperties.$socket = io();
+        //     // app.provide('$socket', app.config.globalProperties.$socket)
+        app.mount("#app");
+    }
 })();
-
-// import VueAnalytics from "vue-analytics";
-
-// import * as filters from "./filters";
-// const filterNames = Object.keys(filters);
-// filterNames.forEach((filter) => Vue.filter(filter, filters[filter]));
-
-// (async () => {
-//     await store.dispatch("initialise");
-//     const configuration = store.state.configuration;
-//     App.router = router({ configuration });
-//     App.store = store;
 
 //     Vue.use(VueAnalytics, {
 //         id: "UA-79571514-3",
@@ -60,6 +48,3 @@ import HTTPService from "./http.service";
 //         },
 //         router: App.router,
 //     });
-
-//     new Vue(App);
-// })();
