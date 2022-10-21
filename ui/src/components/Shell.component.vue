63ebd333c65b262e541302566786cc8107513adf
Marco La Rosa <m@lr.id.au>
2022-10-21 20:14:09 +1100

    push to dashboard if root loaded

--- ui/src/components/Shell.component.vue
+++ ui/src/components/Shell.component.vue
@@ -7,4 +7,9 @@
 
 <script setup>
 import NavbarComponent from "./Navbar.component.vue";
+import { useRoute, useRouter } from "vue-router";
+
+const $route = useRoute();
+const $router = useRouter();
+if ($route.path === "/") $router.replace("/dashboard");
 </script>
