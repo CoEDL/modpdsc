f44b61d07780ae1d6abef5d068884aec83883b8c
Marco La Rosa <m@lr.id.au>
2022-10-21 16:01:40 +1100

    cleanup

--- ui/src/routes.js
+++ ui/src/routes.js
@@ -2,13 +2,7 @@
 
 import { createRouter, createWebHistory } from "vue-router";
 import ShellComponent from "components/Shell.component.vue";
-import BadRequestComponent from "components/BadRequest.component.vue";
-import HealthCheckComponent from "components/HealthCheck.component.vue";
-import AboutComponent from "components/About.component.vue";
-import ExploreComponent from "components/explore/Shell.component.vue";
-import IntroductionComponent from "components/introduction/Shell.component.vue";
 import ViewComponent from "components/view/Shell.component.vue";
-import SupportComponent from "components/support/Shell.component.vue";
 import DashboardComponent from "components/Dashboard.component.vue";
 
 export function router({ configuration }) {
