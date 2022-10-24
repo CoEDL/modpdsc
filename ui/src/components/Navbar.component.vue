3fdb11489be04151e03be331021d66e3e08e7e82
Marco La Rosa <m@lr.id.au>
2022-10-24 15:26:09 +1100

    comment out non existent routes for now

--- ui/src/components/Navbar.component.vue
+++ ui/src/components/Navbar.component.vue
@@ -4,19 +4,19 @@
             <i class="fas fa-home"></i>
             <span class="hidden md:inline">&nbsp;Home</span>
         </router-link>
-        <router-link to="/advanced-search" query="" class="px-5">
+        <!-- <router-link to="/advanced-search" query="" class="px-5">
             <i class="fas fa-search"></i>
             <span class="hidden md:inline">&nbsp;Advanced Search</span>
-        </router-link>
-        <router-link to="/transcription-search" query="" class="px-5">
+        </router-link> -->
+        <!-- <router-link to="/transcription-search" query="" class="px-5">
             <i class="far fa-file"></i>
             <span class="hidden md:inline">&nbsp;Transcription Search</span>
-        </router-link>
+        </router-link> -->
         <div class="flex-grow"></div>
-        <router-link to="/about" query="" class="px-5 hidden md:block">
+        <!-- <router-link to="/about" query="" class="px-5 hidden md:block">
             <i class="fas fa-info-circle"></i>
             <span class="hidden md:inline">&nbsp;About</span>
-        </router-link>
+        </router-link> -->
     </div>
 </template>
 
