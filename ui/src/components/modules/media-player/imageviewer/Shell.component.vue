cc4c70afb49eb98e83922f04b8c99aa03b363a82
Marco La Rosa <m@lr.id.au>
2022-10-31 15:04:54 +1100

    refactor; use zoomist

--- ui/src/components/modules/media-player/imageviewer/Shell.component.vue
+++ ui/src/components/modules/media-player/imageviewer/Shell.component.vue
@@ -1,113 +1,141 @@
 <template>
-    <div class="flex flex-col bg-indigo-100 rounded p-4" v-if="images.length">
-        <div class="flex flex-col md:flex-row md:space-x-2 my-2">
-            <div>{{ selectedName }}</div>
-            <!-- <copy-to-clipboard-component :data="itemLink" /> -->
+    <div class="flex flex-col rounded">
+        <div class="flex flex-col md:flex-row md:space-x-2">
+            <div class="py-2">{{ data.currentImage.name }}</div>
+            <!-- <copy-to-clipboard-component :data="data.currentImage.url" /> -->
             <div class="flex-grow"></div>
             <el-pagination
-                :background="true"
-                layout="prev, pager, next"
+                v-model:currentPage="data.current"
                 :page-size="1"
-                :total="totalImages"
-                :hide-on-single-page="true"
+                layout="total, prev, pager, next"
+                :total="data.imageNames.length"
                 @current-change="update"
-            ></el-pagination>
-            <el-button @click="toggleFullScreen" size="small">
-                <i class="fas fa-expand fa-fw"></i>
-            </el-button>
+            />
         </div>
         <div class="mt-4">
-            <div>
-                <img :src="images[current - 1].url" />
+            <div class="flex flex-col justify-around relative">
+                <div
+                    v-if="data.currentImage.url"
+                    class="absolute cursor-pointer"
+                    style="
+                        top: 98px;
+                        width: 50px;
+                        height: 50px;
+                        right: 0px;
+                        z-index: 10;
+                        background-color: rgba(255, 255, 255, 0.9);
+                    "
+                    @click="refresh"
+                >
+                    <span
+                        class="text-gray-500"
+                        style="
+                            position: absolute;
+                            top: 55%;
+                            left: 43%;
+                            transform: translate(-50%, -50%);
+                            width: 12px;
+                        "
+                    >
+                        <i class="fa-solid fa-sync"></i>
+                    </span>
+                </div>
+                <div :style="maxHeight">
+                    <div id="image" :data-zoomist-src="data.currentImage.url" />
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
 import { getFilesByEncoding, getPresignedUrl } from "../lib";
-import { orderBy, compact, cloneDeep } from "lodash";
+import { groupBy, uniq, orderBy, compact, cloneDeep } from "lodash";
 const { FullScreenViewer } = require("iv-viewer");
-import CopyToClipboardComponent from "@/components/modules/CopyToClipboard.component.vue";
+// import CopyToClipboardComponent from "@/components/modules/CopyToClipboard.component.vue";
+import { reactive, onMounted, nextTick, inject, computed } from "vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+import Zoomist from "zoomist";
+const $http = inject("$http");
+const $store = useStore();
+const $route = useRoute();
+const $emit = defineEmits(["update-route"]);
 
-export default {
-    components: {
-        CopyToClipboardComponent,
+const props = defineProps({
+    crate: {
+        type: Object,
+        required: true,
     },
-    props: {
-        crate: {
-            type: Object,
-            required: true,
-        },
-    },
-    data() {
-        return {
-            images: [],
-            current: 1,
-            totalImages: undefined,
-            selectedName: undefined,
-            itemLink: undefined,
-        };
-    },
-    mounted() {
-        this.init();
-    },
-    methods: {
-        async init() {
-            let images = getFilesByEncoding({
-                crate: this.crate,
-                formats: this.$store.state.configuration.ui.imageFormats,
-            });
-            for (let image of images) {
-                let url = await getPresignedUrl({
-                    $http: this.$http,
-                    identifier: this.$route.params.identifier,
-                    filename: image["@id"],
-                });
-                this.images.push({
-                    ...image,
-                    url,
-                });
-            }
-            this.images = orderBy(this.images, "name");
-            this.totalImages = this.images.length;
-            if (this.$route.hash && this.$route.query?.type === "image") {
-                let images = this.images.map((d) => d.name);
-                this.current = images.indexOf(this.$route.hash.replace("#", "")) + 1;
-            }
-            this.update(this.current);
+});
+const data = reactive({
+    imageNames: [],
+    imagesGroupedByName: {},
+    currentImage: [],
+    current: 1,
+});
+onMounted(() => {
+    init();
+});
+let maxHeight = computed(() => {
+    return { height: `${window.innerHeight - 240}px` };
+});
 
-            // console.log(JSON.stringify(images, null, 2));
-            // const datafiles = cloneDeep(this.data.datafiles);
-            // images = images.map((image) => {
-            //     return {
-            //         ...image,
-            //     };
-            // });
-            // images = compact(images);
-            // this.images = orderBy(images, "name");
-            // if (this.$route.hash && this.$route.query?.type === "image") {
-            //     let images = this.images.map((d) => d.name);
-            //     this.current = images.indexOf(this.$route.hash.replace("#", "")) + 1;
-            // }
-            // this.update(this.current);
-        },
-        update(number) {
-            this.current = number;
-            const file = this.images[this.current - 1];
-            this.selectedName = file.name;
-            this.$emit("update-route", { hash: file.name, type: "image" });
-            this.$nextTick(() => {
-                this.itemLink = window.location;
-            });
-        },
-        toggleFullScreen() {
-            const viewer = new FullScreenViewer({});
-            const file = this.images[this.current - 1];
-            viewer.show(file.path);
-        },
-    },
-};
+async function init() {
+    let images = getFilesByEncoding({
+        crate: props.crate,
+        formats: $store.getters.getConfiguration.ui.imageFormats,
+    });
+
+    data.imagesGroupedByName = groupBy(
+        images,
+        (image) => image["@id"].split("/").pop().split(".")[0]
+    );
+    data.imageNames = uniq(
+        images.map((image) => image["@id"].split("/").pop().split(".")[0])
+    ).sort();
+    update(1);
+}
+
+async function loadImage() {
+    const selectedImageName = data.imageNames[data.current - 1];
+    let images = data.imagesGroupedByName[selectedImageName].filter(
+        (image) => !image["@id"].match(/thumbnail/)
+    );
+    for (let image of images) {
+        let url = await getPresignedUrl({
+            $http,
+            $route,
+            filename: image["@id"],
+        });
+        image.url = url;
+    }
+    data.currentImage = images.filter((image) => image.encodingFormat === "image/jpeg")[0];
+
+    await new Promise((resolve) => setTimeout(resolve, 200));
+    if (!data.zoomist) {
+        data.zoomist = new Zoomist("#image", {
+            fill: "contain",
+            maxRatio: 10,
+            slider: true,
+            zoomer: true,
+            height: window.innerHeight - 240,
+        });
+    } else {
+        data.zoomist.update();
+    }
+}
+
+async function update(number) {
+    data.current = number;
+    await loadImage();
+    $emit("update-route", {
+        contentType: "images",
+        contentId: data.currentImage["@id"],
+    });
+}
+
+function refresh() {
+    data.zoomist.update();
+}
 </script>
-
-<style lang="scss" scoped></style>
