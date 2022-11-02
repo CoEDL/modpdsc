07428d8001248b03dcf0ae8ae7fb1f83de5b1215
Marco La Rosa <m@lr.id.au>
2022-11-02 12:06:13 +1100

    update to composition api and get it working in new mode

--- ui/src/components/modules/media-player/xmlviewer/Shell.component.vue
+++ ui/src/components/modules/media-player/xmlviewer/Shell.component.vue
@@ -1,108 +1,102 @@
 <template>
     <div class="flex flex-col bg-indigo-100 rounded p-4">
         <div class="flex flex-col md:flex-row md:space-x-2 my-2">
-            <div class="" v-if="documents.length">{{ selectedName }}</div>
-            <copy-to-clipboard-component :data="itemLink" />
+            <div class="">{{ data.documentName }}</div>
+            <!-- <copy-to-clipboard-component :data="itemLink" /> -->
             <div class="flex-grow"></div>
             <el-pagination
-                background
-                layout="prev, pager, next"
-                :current-page.sync="current"
-                :pager-count="pagerCount"
-                :total="total"
+                v-model:currentPage="data.current"
                 :page-size="1"
-                @current-change="update"
-            ></el-pagination>
+                layout="total, prev, pager, next"
+                :total="data.total"
+                @current-change="handleCurrentChange"
+            />
         </div>
-        <pre class="style-file-view">
-            <code v-html="fileContent"></code>
+        <div v-if="data.error" class="text-center p-4">There was an error loading that file.</div>
+        <pre class="bg-black overflow-scroll" :style="{ height: panelHeight }" v-if="!data.error">
+            <code v-html="data.fileContent"></code>
         </pre>
     </div>
 </template>
 
-<script>
-import { getFilesByEncoding } from "../lib";
+<script setup>
+import { getFilesByEncoding, getFile } from "../lib";
 import { cloneDeep, compact } from "lodash";
-import { DataLoader } from "src/services/data-loader.service";
+import { reactive, onMounted, computed, inject } from "vue";
+import { useRoute } from "vue-router";
 import CopyToClipboardComponent from "@/components/modules/CopyToClipboard.component.vue";
+const $http = inject("$http");
+const $route = useRoute();
+const $emit = defineEmits(["update-route"]);
 
-const dataLoader = new DataLoader();
+import format from "xml-formatter";
 import Prism from "prismjs";
 Prism.highlightAll();
 
-export default {
-    components: {
-        CopyToClipboardComponent,
+const props = defineProps({
+    crate: {
+        type: Object,
+        required: true,
     },
-    props: {
-        data: {
-            type: Object,
-            required: true,
-        },
-    },
-    data() {
-        return {
-            documents: [],
-            pagerCount: window.innerWidth < 500 ? 5 : 7,
-            total: 0,
-            current: 1,
-            fileContent: "",
-            loading: false,
-            selectedName: undefined,
-            itemLink: undefined,
-        };
-    },
-    mounted() {
-        this.init();
-    },
-    methods: {
-        init() {
-            let documents = getFilesByEncoding({
-                rocrate: this.data.rocrate,
-                formats: ["application/xml"],
-            });
+});
+const data = reactive({
+    error: false,
+    documents: [],
+    pagerCount: window.innerWidth < 500 ? 5 : 7,
+    total: 0,
+    current: 1,
+    fileContent: "",
+    loading: false,
+    documentName: undefined,
+    itemLink: undefined,
+});
+onMounted(() => {
+    init();
+});
+let panelHeight = computed(() => {
+    let subtract = window.innerWidth > 1024 ? 300 : 450;
+    return `${window.innerHeight - subtract}px`;
+});
+function init() {
+    let documents = getFilesByEncoding({
+        crate: props.crate,
+        formats: ["application/xml"],
+    });
+    data.documents = documents;
+    data.total = documents.length;
 
-            const datafiles = cloneDeep(this.data.datafiles);
-            documents = documents.map((d) => {
-                if (!datafiles[d.name]) return undefined;
-
-                return {
-                    ...d,
-                    path: datafiles[d.name].pop().path,
-                };
-            });
-            documents = compact(documents);
-            this.documents = documents;
-            this.total = this.documents.length;
-            if (this.$route.hash && this.$route.query?.type === "xml") {
-                let docs = this.documents.map((d) => d.name);
-                this.current = docs.indexOf(this.$route.hash.replace("#", "")) + 1;
-            }
-            this.highlight();
-        },
-        async highlight() {
-            let file = this.documents[this.current - 1];
-            let data = await load({ file });
-            this.fileContent = Prism.highlight(data, Prism.languages.xml, "xml");
-            this.selectedName = file.name;
-            this.$emit("update-route", { hash: file.name, type: "xml" });
-            this.$nextTick(() => {
-                this.itemLink = window.location;
-            });
-            async function load({ file }) {
-                try {
-                    return await dataLoader.loadFile({ file });
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        },
-        update(number) {
-            this.current = number;
-            this.highlight();
-        },
-    },
-};
+    if ($route.query.file) {
+        const ids = data.documents.map((d) => d["@id"]);
+        data.current = ids.indexOf($route.query.file) + 1;
+    }
+    highlight();
+    updateRoute();
+}
+async function highlight() {
+    data.documentName = data.documents[data.current - 1]["@id"];
+    try {
+        let { content } = await getFile({ $http, $route, filename: data.documentName });
+        data.fileContent = Prism.highlight(format(content.trim()), Prism.languages.xml, "xml");
+        data.error = false;
+    } catch (error) {
+        data.error = true;
+    }
+    // this.$nextTick(() => {
+    //     this.itemLink = window.location;
+    // });
+}
+async function handleCurrentChange(number) {
+    data.current = number;
+    updateRoute();
+    highlight();
+}
+function updateRoute() {
+    const file = data.documents[data.current - 1]["@id"];
+    const route = {
+        contentType: "xml",
+    };
+    $emit("update-route", { ...route, query: { file } });
+}
 </script>
 
 <style lang="scss" scoped>
