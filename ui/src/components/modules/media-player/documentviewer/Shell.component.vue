3324a48c08ce719bc4083f6813fd505d7010b78a
Marco La Rosa <m@lr.id.au>
2022-11-03 09:18:52 +1100

    update document viewer

--- ui/src/components/modules/media-player/documentviewer/Shell.component.vue
+++ ui/src/components/modules/media-player/documentviewer/Shell.component.vue
@@ -1,22 +1,35 @@
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col bg-indigo-100">
         <div class="flex flex-col md:flex-row md:space-x-2 my-3">
-            <div class="text-xl" v-if="documents.length">{{ selectedName }}</div>
-            <copy-to-clipboard-component :data="itemLink" />
+            <div class="p-2">{{ data.documentName }}</div>
+
+            <!-- <copy-to-clipboard-component :data="itemLink" /> -->
             <div class="flex-grow"></div>
             <el-pagination
-                background
-                layout="prev, pager, next"
-                :current-page.sync="current"
-                :pager-count="pagerCount"
-                :total="total"
+                v-model:currentPage="data.current"
                 :page-size="1"
-                @current-change="update"
-            ></el-pagination>
+                layout="total, prev, pager, next"
+                :total="data.total"
+                @current-change="handleCurrentChange"
+            />
         </div>
-        <iframe :src="selectedFileUrl" class="style-file-view">
+        <iframe
+            :src="data.selectedFileUrl"
+            class="overflow-scroll"
+            :style="{ height: panelHeight }"
+        >
             <p>Your browser does not support iframes.</p>
         </iframe>
+        <!-- <iframe
+            :src="data.selectedFileUrl"
+            frameborder="0"
+            class="overflow-scroll"
+            :style="{ height: panelHeight }"
+        >
+            This is an embedded
+            <a target="_blank" href="http://office.com">Microsoft Office</a> document, powered by
+            <a target="_blank" href="http://office.com/webapps">Office Online</a>.
+        </iframe> -->
         <!-- <iframe
     src="https://docs.google.com/viewer?url=http://infolab.stanford.edu/pub/papers/google.pdf&embedded=true"
     style="width:600px; height:500px;"
@@ -25,81 +38,77 @@
     </div>
 </template>
 
-<script>
-import { getFilesByName } from "../lib";
-import { cloneDeep, compact } from "lodash";
-import CopyToClipboardComponent from "@/components/modules/CopyToClipboard.component.vue";
+<script setup>
+import { getFilesByName, getPresignedUrl, panelHeight } from "../lib";
+import { reactive, onMounted, computed, inject } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+// import CopyToClipboardComponent from "@/components/modules/CopyToClipboard.component.vue";
+const $http = inject("$http");
+const $store = useStore();
+const $route = useRoute();
+const $emit = defineEmits(["update-route"]);
 
-export default {
-    components: {
-        CopyToClipboardComponent,
+const props = defineProps({
+    crate: {
+        type: Object,
+        required: true,
     },
-    props: {
-        data: {
-            type: Object,
-            required: true,
-        },
+});
+// Document preview in browser: https://stackoverflow.com/a/27958186
+const data = reactive({
+    documents: [],
+    total: 0,
+    current: 1,
+    selectedFileUrl: undefined,
+    documentName: undefined,
+    itemLink: undefined,
+    viewer: {
+        google: "https://docs.google.com/viewer?url",
+        microsoft: "https://view.officeapps.live.com/op/embed.aspx?src",
     },
-    data() {
-        return {
-            documents: [],
-            pagerCount: window.innerWidth < 500 ? 5 : 7,
-            total: 0,
-            current: 1,
-            fileContent: "",
-            selectedFileUrl: undefined,
-            selectedName: undefined,
-            itemLink: undefined,
-        };
-    },
-    mounted() {
-        this.init();
-    },
-    methods: {
-        init() {
-            let documents = getFilesByName({
-                rocrate: this.data.rocrate,
-                formats: this.$store.state.configuration.documentFileExtensions,
-            });
+});
+onMounted(() => {
+    init();
+});
+function init() {
+    let documents = getFilesByName({
+        crate: props.crate,
+        formats: $store.getters.getConfiguration.ui.documentFileExtensions,
+    });
+    data.documents = documents;
+    data.total = documents.length;
 
-            const datafiles = cloneDeep(this.data.datafiles);
-            documents = documents.map((d) => {
-                if (!datafiles[d.name]) return undefined;
-
-                return {
-                    ...d,
-                    path: datafiles[d.name].pop().path,
-                };
-            });
-            documents = compact(documents);
-            this.documents = documents;
-            this.total = this.documents.length;
-            if (this.$route.hash && this.$route.query?.type === "document") {
-                let docs = this.documents.map((d) => d.name);
-                this.current = docs.indexOf(this.$route.hash.replace("#", "")) + 1;
-            }
-            this.setFile();
-        },
-        setFile() {
-            const file = this.documents[this.current - 1];
-            this.selectedFileUrl = `https://docs.google.com/viewer?url=${window.location.origin}${file.path}&embedded=true`;
-            this.selectedName = file.name;
-            this.$emit("update-route", { hash: file.name, type: "document" });
-            this.$nextTick(() => {
-                this.itemLink = window.location;
-            });
-        },
-        update(number) {
-            this.current = number;
-            this.setFile();
-        },
-    },
-};
-</script>
-
-<style lang="scss" scoped>
-.style-file-view {
-    height: calc(100vh - 250px);
-    width: 100%;
+    if ($route.query.file) {
+        const ids = data.documents.map((d) => d["@id"]);
+        data.current = ids.indexOf($route.query.file) + 1;
+    }
+    setSelectedFile();
 }
-</style>
+async function setSelectedFile() {
+    data.documentName = data.documents[data.current - 1]["@id"];
+    let url = await getPresignedUrl({ $http, $route, filename: data.documentName });
+    url = encodeURIComponent(url);
+    let extension = data.documentName.split(".")[1];
+    // if (["pdf", "rtf", "txt"].includes(extension)) {
+    data.selectedFileUrl = `${data.viewer.google}=${url}&embedded=true`;
+    // } else if (["doc", "docx", "xls", "xlsx", "ppt", "pptx"].includes(extension)) {
+    //     data.selectedFileUrl = `${data.viewer.microsoft}=${url}`;
+    // }
+    // this.$nextTick(() => {
+    //     this.itemLink = window.location;
+    // });
+}
+async function handleCurrentChange(number) {
+    data.current = number;
+    updateRoute({});
+    await setSelectedFile();
+}
+function updateRoute() {
+    const file = data.documents[data.current - 1]["@id"];
+    const route = {
+        contentType: "documents",
+    };
+    $emit("update-route", { ...route, query: { file } });
+}
+</script>
